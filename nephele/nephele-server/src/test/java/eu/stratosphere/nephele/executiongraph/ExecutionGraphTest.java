df0a55dcc8be7ed7ab780335c9c13114f3f90dc7
Robert Metzger <metzgerr@web.de>
2013-09-15 21:01:38 +0200

    Upgrade of maven surefire (integration tests) to most recent version: errors now cause tests to fail too + more precise error messages. Try to fix bug in Exec Graph

--- nephele/nephele-server/src/test/java/eu/stratosphere/nephele/executiongraph/ExecutionGraphTest.java
+++ nephele/nephele-server/src/test/java/eu/stratosphere/nephele/executiongraph/ExecutionGraphTest.java
@@ -848,8 +848,8 @@ public class ExecutionGraphTest {
 			o1.setVertexToShareInstancesWith(o2);
 
 			// connect vertices
-			i1.connectTo(t1, null, DistributionPattern.POINTWISE);
-			i2.connectTo(t2, null, DistributionPattern.POINTWISE);
+			i1.connectTo(t1, ChannelType.INMEMORY, DistributionPattern.POINTWISE);
+			i2.connectTo(t2, ChannelType.INMEMORY, DistributionPattern.POINTWISE);
 			t1.connectTo(t3, ChannelType.NETWORK);
 			t2.connectTo(t3, ChannelType.NETWORK);
 			t3.connectTo(t4, ChannelType.INMEMORY, DistributionPattern.POINTWISE);
@@ -866,7 +866,7 @@ public class ExecutionGraphTest {
 			ExecutionStage executionStage = eg.getCurrentExecutionStage();
 			executionStage.collectRequiredInstanceTypes(instanceRequestMap, ExecutionState.CREATED);
 			assertEquals(1, instanceRequestMap.size());
-			assertEquals(12,
+			assertEquals(8,
 				(int) instanceRequestMap.getMaximumNumberOfInstances(INSTANCE_MANAGER
 					.getInstanceTypeByName(DEFAULT_INSTANCE_TYPE_NAME)));
 			// Fake transition to next stage by triggering execution state changes manually
