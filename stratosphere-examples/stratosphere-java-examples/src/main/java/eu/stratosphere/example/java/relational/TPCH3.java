fc111870bdb081d1d4dd01c209d299e09ee462aa
Sebastian Kunert <skunert49@gmail.com>
2014-04-15 11:23:04 +0200

    removed unnecessary order() methods, updated comments and did cleanup

--- stratosphere-examples/stratosphere-java-examples/src/main/java/eu/stratosphere/example/java/relational/TPCH3.java
+++ stratosphere-examples/stratosphere-java-examples/src/main/java/eu/stratosphere/example/java/relational/TPCH3.java
@@ -21,55 +21,55 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-import eu.stratosphere.api.common.operators.Ordering;
+import org.apache.tools.ant.types.CommandlineJava.SysProperties;
+
 import eu.stratosphere.api.java.DataSet;
 import eu.stratosphere.api.java.ExecutionEnvironment;
 import eu.stratosphere.api.java.functions.FilterFunction;
 import eu.stratosphere.api.java.functions.JoinFunction;
-import eu.stratosphere.api.java.functions.KeySelector;
 import eu.stratosphere.api.java.functions.MapFunction;
 import eu.stratosphere.api.java.functions.ReduceFunction;
 import eu.stratosphere.api.java.tuple.Tuple2;
 import eu.stratosphere.api.java.tuple.Tuple3;
 import eu.stratosphere.api.java.tuple.Tuple4;
 import eu.stratosphere.api.java.tuple.Tuple5;
-import eu.stratosphere.api.java.tuple.Tuple7;
 import eu.stratosphere.configuration.Configuration;
 
 /**
- * This program implements a modified version of the TPC-H query 3. Order by ist not yet supported.
- * 
- * The original query can be found at
- * http://www.tpc.org/tpch/spec/tpch2.16.0.pdf (page 29).
- * 
- * This program implements the following SQL equivalent:
- * 
- * select l_orderkey, 
- * 		  sum(l_extendedprice*(1-l_discount)) as revenue,
- * 		  o_orderdate, 
- *        o_shippriority from customer, 
- *        orders, 
- *        lineitem 
- * where
- * 		  c_mktsegment = '[SEGMENT]' and 
- * 		  c_custkey = o_custkey and 
- * 		  l_orderkey = o_orderkey and
- *        o_orderdate < date '[DATE]' and 
- *        l_shipdate > date '[DATE]'
- * group by 
- * 		  l_orderkey, 
- * 	      o_orderdate, 
- *        o_shippriority 
- * order by  					//not yet
- * 		  revenue desc,
- *        o_orderdate;
- * 
+ *	This program implements a modified version of the TPC-H query 3. The
+ *	example demonstrates how to assign names to fields by extending the Tuple class.
+ *
+ *	The original query can be found at
+ *	http://www.tpc.org/tpch/spec/tpch2.16.0.pdf (page 29).
+ *
+ *	This program implements the following SQL equivalent:
+ *
+ *	select l_orderkey, 
+ *		sum(l_extendedprice*(1-l_discount)) as revenue,
+ *		o_orderdate, 
+ *		o_shippriority from customer, 
+ *		orders, 
+ *		lineitem 
+ *	where
+ *		c_mktsegment = '[SEGMENT]' and 
+ *		c_custkey = o_custkey and 
+ *		l_orderkey = o_orderkey and
+ *		o_orderdate < date '[DATE]' and 
+ *		l_shipdate > date '[DATE]'
+ *	group by 
+ *		l_orderkey, 
+ *		o_orderdate, 
+ *		o_shippriority 
+ *	order by  					//not yet
+ *		revenue desc,
+ *		o_orderdate;
+ *
  */
+@SuppressWarnings("serial")
 public class TPCH3 {
 	public static class Lineitem extends Tuple4<Integer, Double, Double, String> {
 
 		public Lineitem() {
-			// default constructor
 		}
 
 		public Lineitem(Integer l_orderkey, Double l_extendedprice,
@@ -84,32 +84,16 @@ public class TPCH3 {
 			return this.f0;
 		}
 
-		public void setOrderkey(Integer l_orderkey) {
-			this.f0 = l_orderkey;
-		}
-
 		public Double getDiscount() {
 			return this.f2;
-		}
-
-		public void setDiscount(Double l_discount) {
-			this.f2 = l_discount;
 		}
 
 		public Double getExtendedprice() {
 			return this.f1;
 		}
 
-		public void setExtendedprice(Double l_extendedprice) {
-			this.f1 = l_extendedprice;
-		}
-
 		public String getShipdate() {
 			return this.f3;
-		}
-
-		public void setShipdate(String l_shipdate) {
-			this.f3 = l_shipdate;
 		}
 	}
 
@@ -130,20 +114,11 @@ public class TPCH3 {
 		public String getMktsegment() {
 			return this.f1;
 		}
-		
-		public void setCustKey(Integer c_custkey) {
-			this.f0 = c_custkey;
-		}
-		
-		public void setMktsegment(String c_mktsegment) {
-			this.f1 = c_mktsegment;
-		}
 	}
 
 	public static class Order extends Tuple3<Integer, String, Integer>{
 
 		public Order() {
-			// default constructor
 		}
 
 		public Order(Integer o_orderkey, String o_orderdate,
@@ -157,31 +132,18 @@ public class TPCH3 {
 			return this.f0;
 		}
 
-		public void setOrderkey(Integer o_orderkey) {
-			this.f0 = o_orderkey;
-		}
-
 		public String getOrderdate() {
 			return this.f1;
 		}
 
-		public void setOrderdate(String o_orderdate) {
-			this.f1 = o_orderdate;
-		}
-
 		public Integer getShippriority() {
 			return this.f2;
-		}
-
-		public void setShippriority(Integer o_shippriority) {
-			this.f2 = o_shippriority;
 		}
 	}
 
 	public static class ShippingPriorityItem extends Tuple5<Integer, Double, String, Integer, Integer> {
 
 		public ShippingPriorityItem() {
-			// default constructor
 		}
 
 		public ShippingPriorityItem(Integer l_orderkey, Double revenue,
@@ -197,7 +159,7 @@ public class TPCH3 {
 			return this.f0;
 		}
 
-		public void setOrderkey(Integer l_orderkey) {
+		public void setL_Orderkey(Integer l_orderkey) {
 			this.f0 = l_orderkey;
 		}
 
@@ -213,35 +175,18 @@ public class TPCH3 {
 			return this.f2;
 		}
 
-		public void setOrderdate(String o_orderdate) {
-			this.f2 = o_orderdate;
-		}
-
 		public Integer getShippriority() {
 			return this.f3;
-		}
-
-		public void setShippriority(Integer o_shippriority) {
-			this.f3 = o_shippriority;
 		}
 
 		public Integer getO_Orderkey() {
 			return this.f4;
 		}
-
-		public void setO_Orderkey(Integer o_orderkey) {
-			this.f4 = o_orderkey;
-		}
-
-		@Override
-		public String toString() {
-			return this.f0 + "; " + this.f1 + "; " + this.f2 + "; " + this.f3 + "; " + this.f4;
-		}
 	}
 	/*
 	 * This example TPCH3 query uses custom objects.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		final String lineitemPath;
 		final String customerPath;
 		final String ordersPath;
@@ -255,9 +200,7 @@ public class TPCH3 {
 			ordersPath = args[2];
 		}
 
-		final ExecutionEnvironment env = ExecutionEnvironment
-				.getExecutionEnvironment();
-		env.setDegreeOfParallelism(1);
+		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
 		/*
 		 * Read Data from files
@@ -266,9 +209,11 @@ public class TPCH3 {
 				.readCsvFile(lineitemPath).fieldDelimiter('|')
 				.includeFields("1000011000100000")
 				.types(Integer.class, Double.class, Double.class, String.class);
+		
 		DataSet<Tuple2<Integer, String>> customers = env
 				.readCsvFile(customerPath).fieldDelimiter('|')
 				.includeFields("10000010").types(Integer.class, String.class);
+		
 		DataSet<Tuple3<Integer, String, Integer>> orders = env
 				.readCsvFile(ordersPath).fieldDelimiter('|')
 				.includeFields("100010010")
@@ -279,9 +224,6 @@ public class TPCH3 {
 		 */
 		DataSet<Lineitem> li = lineitems
 				.map(new MapFunction<Tuple4<Integer, Double, Double, String>, Lineitem>() {
-
-					private static final long serialVersionUID = 1L;
-
 					@Override
 					public Lineitem map(
 							Tuple4<Integer, Double, Double, String> value)
@@ -294,11 +236,8 @@ public class TPCH3 {
 		/*
 		 * Convert Tuple DataSet to Order Dataset
 		 */
-		DataSet<Order> or;
-			or = orders
+		DataSet<Order> or = orders
 					.map(new MapFunction<Tuple3<Integer, String, Integer>, Order>() {
-						private static final long serialVersionUID = 1L;
-
 						@Override
 						public Order map(Tuple3<Integer, String, Integer> value)
 								throws Exception {
@@ -311,11 +250,6 @@ public class TPCH3 {
 		 */
 		DataSet<Customer> cust = customers
 				.map(new MapFunction<Tuple2<Integer, String>, Customer>() {
-					/**
-					 * 
-					 */
-					private static final long serialVersionUID = 1L;
-
 					@Override
 					public Customer map(Tuple2<Integer, String> value)
 							throws Exception {
@@ -327,10 +261,8 @@ public class TPCH3 {
 		 * Filter market segment "AUTOMOBILE"
 		 */
 		cust = cust.filter(new FilterFunction<Customer>() {
-			private static final long serialVersionUID = 1L;
-
 			@Override
-			public boolean filter(Customer value) throws Exception {
+			public boolean filter(Customer value) {
 				
 				return value.getMktsegment().equals("AUTOMOBILE");
 			}
@@ -340,22 +272,19 @@ public class TPCH3 {
 		 * Filter all Orders with o_orderdate < 12.03.1995
 		 */
 		or = or.filter(new FilterFunction<Order>() {
-			private static final long serialVersionUID = 1L;
-			private  DateFormat format;
-			private Calendar cal;
+			private DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+			private Date date;
 			
-			@Override
-			public void open(Configuration parameters) throws Exception {
-				super.open(parameters);
-				format = new SimpleDateFormat("yyyy-MM-dd");
-				cal = Calendar.getInstance();
+			{
+				Calendar cal = Calendar.getInstance();
 				cal.set(1995, 3, 12);
+				date = cal.getTime();
 			}
 			
 			@Override
-			public boolean filter(Order value) throws Exception {
+			public boolean filter(Order value) throws ParseException {
 				Date orderDate = format.parse(value.getOrderdate());
-				return orderDate.before(cal.getTime());
+				return orderDate.before(date);
 			}
 		});
 		
@@ -363,22 +292,19 @@ public class TPCH3 {
 		 * Filter all Lineitems with l_shipdate > 12.03.1995
 		 */
 		li = li.filter(new FilterFunction<Lineitem>() {
-			private static final long serialVersionUID = 1L;
-			private DateFormat format;
-			private Calendar cal;
+			private DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+			private Date date;
 			
-			@Override
-			public void open(Configuration parameters) throws Exception {
-				super.open(parameters);
-				format = new SimpleDateFormat("yyyy-MM-dd");
-				cal = Calendar.getInstance();
+			{
+				Calendar cal = Calendar.getInstance();
 				cal.set(1995, 3, 12);
+				date = cal.getTime();
 			}
 			
 			@Override
-			public boolean filter(Lineitem value) throws Exception {
+			public boolean filter(Lineitem value) throws ParseException {
 				Date shipDate = format.parse(value.getShipdate());
-				return shipDate.after(cal.getTime());
+				return shipDate.after(date);
 			}
 		});
 
@@ -390,8 +316,6 @@ public class TPCH3 {
 				.where(0)
 				.equalTo(0)
 				.with(new JoinFunction<Customer, Order, ShippingPriorityItem>() {
-					private static final long serialVersionUID = 1L;
-
 					@Override
 					public ShippingPriorityItem join(Customer first,
 							Order second) throws Exception {
@@ -410,15 +334,12 @@ public class TPCH3 {
 				.where(4)
 				.equalTo(0)
 				.with(new JoinFunction<ShippingPriorityItem, Lineitem, ShippingPriorityItem>() {
-
-					private static final long serialVersionUID = 1L;
-
 					@Override
 					public ShippingPriorityItem join(
 							ShippingPriorityItem first, Lineitem second)
 							throws Exception {
 						
-						first.setOrderkey(second.getOrderkey());
+						first.setL_Orderkey(second.getOrderkey());
 						first.setRevenue(second.getExtendedprice() * (1 - second.getDiscount()));
 						return first;
 					}
@@ -430,23 +351,18 @@ public class TPCH3 {
 		 */
 		joined = joined
 				.groupBy(0, 2, 3)
-				.reduce(new ReduceFunction<TPCH3.ShippingPriorityItem>() {
-				private static final long serialVersionUID = 1L;
-	
+				.reduce(new ReduceFunction<TPCH3.ShippingPriorityItem>() {	
 					@Override
 					public ShippingPriorityItem reduce(ShippingPriorityItem value1,
 							ShippingPriorityItem value2) throws Exception {
 						value1.setRevenue(value1.getRevenue() + value2.getRevenue());
 						return value1;
 					}
-			});
+				});
 		
-			try {
-				joined.print();
-				env.execute();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		joined.print();
+		env.execute();
+
 	
 	}
 	
